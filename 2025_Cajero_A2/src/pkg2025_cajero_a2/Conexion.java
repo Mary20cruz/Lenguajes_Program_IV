a141bd240ccf93ead5a00564d7096f295833faba
Mary20cruz <139106351+Mary20cruz@users.noreply.github.com>
2025-04-20 19:50:42 -0600

    Commit Actividad3

---
+++ 2025_Cajero_A2/src/pkg2025_cajero_a2/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2025_cajero_a2;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager; 
+
+/**
+ *
+ * @author mcruz
+ */
+public class Conexion {
+    
+    public Connection getConnection (){
+        
+        Connection con = null;
+        String base = "bancobd";
+        String url = "jdbc:mysql://localhost:3306/" + base;
+        String user = "root";
+        String password = "";
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(url, user, password);
+        } catch(Exception e) {
+            System.err.print(e);
+        }
+        return con;
+    }
+      
+}
